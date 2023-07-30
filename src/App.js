a636272aef48d459bf88f588df4dadf4c9e3be77
AnkitChauhan39 <110143320+AnkitChauhan39@users.noreply.github.com>
2023-07-30 22:55:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css';
+import data from "./data"
+import Tours from './COmponents/Tours';
+
+function App() {
+
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+    console.log("hello");
+  }
+
+
+  if (tours.length === 0) {
+    return (
+
+      <div className="refresh" >
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}>
+          REFRESH
+        </button>
+      </div>
+
+    );
+  }
+
+  return (
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour} ></Tours>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/COmponents/Navbar.js
@@ -0,0 +1,13 @@
+
+function navBar()
+{
+    
+    return(
+        <div className="nav">
+            <div className="logo"></div>
+        </div>
+    );
+}
+
+export default navBar ;
+
